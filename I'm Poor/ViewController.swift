6e1a1379fc0ee2ff93f1d77a712096880987069f
Smartlive <cesarmanrg@gmail.com>
2018-11-22 22:15:59 -0600

    Proyecto del curso iOS 12

--- I'm Poor/ViewController.swift
+++ I'm Poor/ViewController.swift
@@ -2,19 +2,30 @@
 //  ViewController.swift
 //  I'm Poor
 //
-//  Created by Smartlive on 11/22/18.
-//  Copyright © 2018 Mextours. All rights reserved.
+//  Created by Cesar Manuel Rios Gomez on 11/22/18.
+//  Copyright © 2018 Cesar  Manuel Rios Gomez. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ULPoor: UILabel!
+    @IBOutlet weak var UIPoor: UIImageView!
+    @IBOutlet weak var UIBRich: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func Rich(_ sender: UIButton) {
+       
+        self.ULPoor.text = "¡SOY RICO!"
+        self.UIPoor.image = UIImage(named: "billeteradinero")
+    }
+    
+    
+    
 }
 
