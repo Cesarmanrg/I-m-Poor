0502f91ec7fb56d5ed6213b6c974f00b213dfb0d
Smartlive <cesarmanrg@gmail.com>
2018-11-22 21:07:02 -0600

    Initial Commit

---
+++ I'm Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I'm Poor
+//
+//  Created by Smartlive on 11/22/18.
+//  Copyright © 2018 Mextours. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
